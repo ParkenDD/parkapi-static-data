41e4575f1688edbfb4442eef95af3329389d1667
Thorsten Fröhlinghaus <120580045+ThorstenFroehlinghaus@users.noreply.github.com>
2024-10-30 17:25:32 +0100

    simplify code and remove UTF-8 defaults

--- csv2geojson.py
+++ csv2geojson.py
@@ -1,37 +1,38 @@
 import sys
 import csv
 import json
+from pathlib import Path
 
 argv = sys.argv[1:]
 if len(argv) == 0:
-    print("no csv file specified")
+    print("csv2geojson: missing csv file")
     sys.exit(2)
-csvfile = argv[0]
+csvfile = Path(argv[0])
 
 features = []
-with open(csvfile, 'r', encoding='utf-8') as file:
+with csvfile.open('r') as file:
     reader = csv.DictReader(file)
     for row in reader:
-        if 'uid' not in row or row['uid'] == '' :
+        if not row.get('uid'):
             print(f'Attribute \'uid\' missing in row {row}')
             continue
-        if 'lat' not in row or 'lon' not in row:
+        if not row.get('lat') or not row.get('lon'):
             print(f'Attributes \'lat\'/\'lon\' missing in row {row}')
             continue
         properties = { 'uid': row['uid'] }
         for column in ['address', 'type']:
-            if column in row and row[column] != '':
+            if row.get(column):
                 properties[column] = row[column]
         for column in ['max_height', 'max_width', 'max_depth']:
-            if column in row and row[column] != '' and row[column].isdigit():
+            if row.get(column) and row[column].isdigit():
                 properties[column] = int(row[column])
-        if 'park_and_ride_type' in row and row['park_and_ride_type'] != '':
+        if row.get('park_and_ride_type'):
             properties['park_and_ride_type'] = [row['park_and_ride_type']]
-        if 'DHID' in row and row['DHID'] != '':
+        if row.get('DHID'):
             properties['external_identifiers'] = { 'type': 'DHID', 'value': row['DHID'] }
         features.append({ 'type': 'Feature', 'properties': properties, 'geometry': { 'type': 'Point', 'coordinates': [float(row['lon']), float(row['lat'])] } })
 
-jsonfile = csvfile.replace('.csv', '.geojson')
 jsondata = { 'type': 'FeatureCollection', 'features': features }
-with open(jsonfile, 'w', encoding='utf-8') as file:
+jsonfile = csvfile.with_suffix('.geojson')
+with jsonfile.open('w') as file:
     json.dump(jsondata, file, ensure_ascii=False, indent=4)

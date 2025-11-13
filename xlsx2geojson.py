981fff9dd609e86a6b384670e2cd612b44be1560
Abdullahi Fatola <fatolaabdullahi@outlook.com>
2025-11-13 13:50:31 +0100

    change restricted_to to restrictions

--- xlsx2geojson.py
+++ xlsx2geojson.py
@@ -397,6 +397,7 @@ class Xlsx2GeojsonParkingSpots(
                         parking_spot_dict
                     ).to_dict()
                 )
+                print(static_parking_spot_input)
                 static_parking_spot_features.append(
                     {
                         "type": "Feature",
@@ -452,7 +453,7 @@ class Xlsx2GeojsonParkingSpots(
 
         restricted_to_raw = parking_spot_raw_dict.get("restricted_to_type", "")
         restricted_to_type = (
-            self.restricted_to_type_mapping.get(restricted_to_raw.strip())
+            self.restricted_to_type_mapping.get(restricted_to_raw.lower().strip())
             if isinstance(restricted_to_raw, str)
             else None
         )
@@ -468,7 +469,7 @@ class Xlsx2GeojsonParkingSpots(
             else None
         )
 
-        parking_spot_dict["restricted_to"] = [restricted_to]
+        parking_spot_dict["restrictions"] = [restricted_to]
         parking_spot_dict["has_realtime_data"] = self.source_info.has_realtime_data
         parking_spot_dict["purpose"] = self.purpose_type_mapping.get(
             normalize_text(parking_spot_dict.get("purpose"))
